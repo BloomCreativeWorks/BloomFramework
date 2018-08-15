c383c43868a23fb13a54c4a0f3c7801147773fdc
Derrick Timmermans <derrick.timmermans@outlook.com>
2018-08-15 22:50:33 +0800

    Added basic functionality
    Includes basic bitmap loading, simple event handling

---
+++ BloomFramework/src/InputHandler.cpp
@@ -0,0 +1,31 @@
+#include "../inc/framework.h"
+using namespace BloomFramework;
+
+Events BloomFramework::InputHandler::getInput() {
+	//Handle events on queue
+	while(SDL_PollEvent(&e) != 0) {
+		//User requests quit
+		if(e.type == SDL_QUIT) {
+			return quit;
+		}
+		//User presses a key
+		else if(e.type == SDL_KEYDOWN) {
+			//Select surfaces based on key press
+			switch(e.key.keysym.sym) {
+			case SDLK_UP:
+				return up;
+			case SDLK_DOWN:
+				return down;
+			case SDLK_LEFT:
+				return left;
+			case SDLK_RIGHT:
+				return right;
+			default:
+				return empty;
+			}
+		}
+		else {
+			return empty;
+		}
+	}
+}
--- Test Bench/main.cpp
+++ Test Bench/main.cpp
@@ -1,8 +1,55 @@
-#include <framework.h>
+#include "framework.h"
 
 int main() {
 	BloomFramework::GraphicsWindow myWindow;
-	myWindow.initWindow();
+
+	myWindow.setWindowTitle("Graphics Window");
+	myWindow.setHeight(337);
+	myWindow.setWidth(600);
+	if(!myWindow.initWindow()) {
+		std::clog << "[ERROR] Failed to initialize window!" << std::endl;
+	} else {
+		if(!myWindow.loadMedia("Resources/test.bmp")) {
+			std::clog << "[ERROR] Failed to load image!" << std::endl;
+		}
+	}
+
+	bool quit = false;
+
+	BloomFramework::InputHandler input;
+	while(!quit) {
+		myWindow.updateWindow();
+		//Handle events on queue
+		BloomFramework::Events event = input.getInput();
+		switch(event) {
+			using namespace BloomFramework;
+		case Events::quit:
+			quit = true;
+			break;
+		case Events::up:
+			if(!myWindow.loadMedia("Resources/up.bmp")) {
+				std::clog << "[ERROR] Failed to load image!" << std::endl;
+			}
+			break;
+		case Events::down:
+			if(!myWindow.loadMedia("Resources/down.bmp")) {
+				std::clog << "[ERROR] Failed to load image!" << std::endl;
+			}
+			break;
+		case Events::left:
+			if(!myWindow.loadMedia("Resources/left.bmp")) {
+				std::clog << "[ERROR] Failed to load image!" << std::endl;
+			}
+			break;
+		case Events::right:
+			if(!myWindow.loadMedia("Resources/right.bmp")) {
+				std::clog << "[ERROR] Failed to load image!" << std::endl;
+			}
+			break;
+		}
+	}
+
+	myWindow.destroyWindow();
 
 	return 0;
 }
