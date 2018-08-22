9d3df109303d6ff97d4a84070672e5d1f3b2f903
Derrick Timmermans <derrick.timmermans@outlook.com>
2018-08-22 23:32:58 +0800

    Accidentally uploaded old code

---
+++ BloomFramework/src/framework.cpp
@@ -0,0 +1,58 @@
+#include "../inc/framework.h"
+
+BloomFramework::Game::Game() {}
+
+BloomFramework::Game::~Game() {}
+
+void BloomFramework::Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen) {
+	int flags = 0;
+
+	if(fullscreen) {
+		flags = SDL_WINDOW_FULLSCREEN;
+	}
+
+	if(!SDL_Init(SDL_INIT_EVERYTHING)) {
+
+		std::clog << "Subsystems initialized!" << std::endl;
+
+		window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
+		renderer = SDL_CreateRenderer(window, -1, 0);
+		if(renderer) {
+			SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+			std::clog << "renderer initialized!" << std::endl;
+		}
+
+		isRunning = true;
+	}
+}
+
+void BloomFramework::Game::handleEvents() {
+	SDL_Event event;
+
+	SDL_PollEvent(&event);
+
+	switch(event.type) {
+	case SDL_QUIT:
+		isRunning = false;
+		break;
+	default:
+		break;
+	}
+}
+
+void BloomFramework::Game::update() {
+	counter++;
+	std::cout << counter << std::endl;
+}
+
+void BloomFramework::Game::render() {
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+}
+
+void BloomFramework::Game::clean() {
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+	std::clog << "Cleaned window!" << std::endl;
+}
