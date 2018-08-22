56e3a558d68bae2b1c1c3d965fba893e7bbab3a2
Derrick Timmermans <derrick.timmermans@outlook.com>
2018-08-22 17:00:32 +0800

    Removed redundant blank lines

--- Test Bench/main.cpp
+++ Test Bench/main.cpp
@@ -4,16 +4,13 @@ using namespace BloomFramework;
 Game* game = nullptr;
 
 int main() {
-
 	game = new Game();
 	game->init("BloomFramework Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, true);
 	while(game->running()) {
 		game->handleEvents();
 		game->update();
 		game->render();
-
 	}
 	game->clean();
 	return 0;
-
 }
