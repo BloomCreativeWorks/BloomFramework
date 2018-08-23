84dc3dabda68f5d58f94869e89b256458dac926c
Derrick Timmermans <derrick.timmermans@outlook.com>
2018-08-23 15:56:30 +0800

    Limit the test game framerate to 60
    Will implement frame limit and vsync in the engine in the future.

--- Test Bench/main.cpp
+++ Test Bench/main.cpp
@@ -1,15 +1,26 @@
-#include "framework.h"
+#include "Framework.h"
 using namespace BloomFramework;
 
 Game* game = nullptr;
 
 int main() {
+	const int fps = 60;
+	const int framedelay = 1000 / fps;
+
+	Uint32 framestart;
+	int frametime;
+
 	game = new Game();
-	game->init("BloomFramework Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, true);
+	game->init("BloomFramework Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 	while(game->running()) {
+		framestart = SDL_GetTicks();
 		game->handleEvents();
 		game->update();
 		game->render();
+		frametime = SDL_GetTicks() - framestart;
+
+		if(framedelay > frametime)
+			SDL_Delay(framedelay - frametime);
 	}
 	game->clean();
 	return 0;
