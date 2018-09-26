510e0ac2c1f965897f9d0115e4f4683d3d7e1453
Derrick Timmermans <derrick.timmermans@outlook.com>
2018-09-26 21:14:53 +0800

    Add functions to add and remove components

--- BloomFramework/include/GameObject.h
+++ BloomFramework/include/GameObject.h
@@ -1,12 +1,22 @@
 #pragma once
 
 #include "stdIncludes.h"
+#include "Components/Components.h"
 
 namespace bloom {
 	class GameObject {
 	public:
 		GameObject(entt::DefaultRegistry & registry);
 		~GameObject();
+
+		template <typename Component, typename... Args>
+		void assignComponent(Args &&... arguments) {
+			m_registry.assign<Component>(m_entity, std::forward<Args>(arguments));
+		}
+		template <typename Component>
+		void removeComponent() {
+			m_registry.reset<Component>(m_entity);
+		}
 
 	protected:
 		entt::DefaultRegistry & m_registry;
--- BloomFramework/src/GameObject.cpp
+++ BloomFramework/src/GameObject.cpp
@@ -1,5 +1,12 @@
 #include "GameObject.h"
 
 namespace bloom {
-	
+	GameObject::GameObject(entt::DefaultRegistry & registry) : m_registry(registry) {
+		m_entity = m_registry.create();
+		m_registry.assign<Position>(m_entity, 0.0f, 0.0f); // This is a must-have in most cases.
+	}
+	GameObject::~GameObject()
+	{
+		m_registry.destroy(m_entity);
+	}
 }
