ee56e4430bdaea72e127d9cfc289a2b2b5f067e6
xapdkop <xapdkop@xapdkop.space>
2018-10-15 16:50:30 +0300

    Add another one blank line

--- BloomFramework/include/Components/Sprite.h
+++ BloomFramework/include/Components/Sprite.h
@@ -8,6 +8,7 @@ using namespace bloom::graphics;
 namespace bloom::components {
 	struct Sprite {
 		Sprite(TexturePtr texture, std::optional<SDL_Rect> srcRect = std::nullopt) : _texture(texture), _srcRect(srcRect) {}
+
 		TexturePtr _texture;
 		std::optional<SDL_Rect> _srcRect;
 	};
