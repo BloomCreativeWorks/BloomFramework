2fd464730136b706351f5f0ef408492902186626
xapdkop <xapdkop@xapdkop.space>
2018-10-23 12:41:18 +0300

    use utf8 strings (as sdl2 works with utf8)

--- BloomFramework/src/Graphics/TextureStore.cpp
+++ BloomFramework/src/Graphics/TextureStore.cpp
@@ -9,12 +9,12 @@ namespace bloom::graphics {
 
 	TexturePtr TextureStore::load(const std::filesystem::path & filePath, std::optional<SDL_Color> colorKey) {
 		// Check if texture of the same path is loaded. If so, return shared_ptr of texture.
-		auto textureIt = m_store.find(filePath.string());
+		auto textureIt = m_store.find(filePath.u8string());
 		if (textureIt != m_store.end())
 			return textureIt->second;
 
 		//Load image at specified path
-		SDL_Surface * loadedSurface = IMG_Load(filePath.string().c_str());
+		SDL_Surface * loadedSurface = IMG_Load(filePath.u8string().c_str());
 		if (loadedSurface == nullptr)
 		{
 			throw Exception("[SDL_IMG] " + std::string(SDL_GetError()));
@@ -32,7 +32,7 @@ namespace bloom::graphics {
 			}
 			else {
 				TexturePtr ptr = std::make_shared<Texture>(texture, m_renderer);
-				m_store.emplace(filePath.string(), ptr);
+				m_store.emplace(filePath.u8string(), ptr);
 				SDL_FreeSurface(loadedSurface);
 				return ptr;
 			}
@@ -40,7 +40,7 @@ namespace bloom::graphics {
 	}
 
 	TexturePtr TextureStore::find(const std::filesystem::path & filePath) {
-		auto texIterator = m_store.find(filePath.string());
+		auto texIterator = m_store.find(filePath.u8string());
 		if (texIterator != m_store.end())
 			return texIterator->second;
 		else {
@@ -49,7 +49,7 @@ namespace bloom::graphics {
 	}
 
 	TexturePtr TextureStore::find(std::nothrow_t, const std::filesystem::path & filePath) {
-		auto texIterator = m_store.find(filePath.string());
+		auto texIterator = m_store.find(filePath.u8string());
 		if (texIterator != m_store.end())
 			return texIterator->second;
 		else {
@@ -58,7 +58,7 @@ namespace bloom::graphics {
 	}
 
 	void TextureStore::unload(const std::filesystem::path & filePath) {
-		auto texIterator = m_store.find(filePath.string());
+		auto texIterator = m_store.find(filePath.u8string());
 		if (texIterator != m_store.end())
 			m_store.erase(texIterator); // We can't dispose the actual Texture since other's may still be using it.
 	}
