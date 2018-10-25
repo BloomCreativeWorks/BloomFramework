f960008991bf6d2560e44bfb7aa49782cc5ef8a2
Derrick Timmermans <derrick.timmermans@outlook.com>
2018-10-25 17:18:55 +0800

    Re-enable randomizer system in Test Bench
    Maybe a little bit wonky due to new positioning system

--- Test Bench/TestScreen.h
+++ Test Bench/TestScreen.h
@@ -11,9 +11,9 @@ public:
 	void Screen::init() {
 		addGameObject<TestChar>("testSprite", Position( 10,10 ), Size( 128,128 ), "Assets/OverworldTestSpritesheet.png", SDL_Rect{ 0,0,32,32 });
 		addGameObject<TestChar>("testSprite2", Position(0, 0, relative), Size(128, 128), "Assets/TestChar.png", SDL_Rect{ 0, 0, 32, 32 },2);
-		addGameObject<TestChar>("testGO", Position(50, 50, relative, (middle|right)), Size(128, 128), "Assets/TestChar.png", SDL_Rect{ 64, 96, 32, 32 },3);
+		addGameObject<TestChar>("testGO", Position(50, 50, relative, (middle|right)), Size(256, 256), "Assets/TestChar.png", SDL_Rect{ 64, 96, 32, 32 },3);
 
-		//registerSystem<RandomPositionSystem>();
+		registerSystem<RandomPositionSystem>();
 		registerSystem<RenderSystem>();
 	}
 };
