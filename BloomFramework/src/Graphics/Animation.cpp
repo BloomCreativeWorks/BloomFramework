1dac9fb9af4f06013d0a01de3b1fd78baccba8c9
Anatoly Pitikin <xapdkop@xapdkop.space>
2018-12-30 17:24:42 +0300

    [Animation] Remove std::fabs for deltaTime in update

--- BloomFramework/src/Graphics/Animation.cpp
+++ BloomFramework/src/Graphics/Animation.cpp
@@ -5,7 +5,7 @@ namespace bloom::graphics {
 	Sprite Animation::update(double deltaTime) {
 		if (animationFrames.empty())
 			throw Exception("[Animation::update] there are no any frames");
-		m_lastUpdateTime += std::fabs(deltaTime);
+		m_lastUpdateTime += deltaTime;
 		if (m_lastUpdateTime > m_frameTime) {
 			m_currentFrame = (m_currentFrame + static_cast<int>(m_lastUpdateTime / m_frameTime + 0.5)) % animationFrames.size();
 			m_lastUpdateTime = std::fmod(m_lastUpdateTime, m_frameTime);
