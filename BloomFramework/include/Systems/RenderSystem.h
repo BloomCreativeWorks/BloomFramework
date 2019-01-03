d96e026eec165d05c73b662e83f5b3465e47bf71
Anatoly Pitikin <xapdkop@xapdkop.space>
2019-01-03 22:10:05 +0300

    [RenderSystem.h] Small fixes and simplifications
    - Use std::list instead of std::vector
    - Remove useless casts
    - Simplify LayerNo initialization
    - Use stable_sort instead of sort
    - Use structured bindings for tuples

--- BloomFramework/include/Systems/RenderSystem.h
+++ BloomFramework/include/Systems/RenderSystem.h
@@ -18,10 +18,10 @@ namespace bloom::systems {
 		~RenderSystem() = default;
 
 		void update(std::optional<double> deltaTime = std::nullopt) override {
-			std::vector<std::tuple<Sprite,Rotation, SDL_Rect, LayerGroup>> renderQueue{};
+			std::list<std::tuple<Sprite, Rotation, SDL_Rect, LayerGroup>> renderQueue;
 
 			m_registry.view<Transform, Sprite>().each(
-				[&](auto entity, Transform & trans, Sprite & spr) {
+				[&](auto entity, Transform& trans, Sprite& spr) {
 
 				if (trans.size.w < 0)
 					trans.size.w = 0;
@@ -30,37 +30,29 @@ namespace bloom::systems {
 
 				Coord actualPos = trans.position.getSDLPos(parentScene.getGameInstance().getRenderer(), trans.size.w, trans.size.h);
 				SDL_Rect destRect{
-					static_cast<int>(actualPos.x),
-					static_cast<int>(actualPos.y),
-					static_cast<int>(trans.size.w),
-					static_cast<int>(trans.size.h)
+					actualPos.x,
+					actualPos.y,
+					trans.size.w,
+					trans.size.h
 				};
 
-				LayerGroup layerNo;
-				if (m_registry.has<LayerGroup>(entity))
-					layerNo = m_registry.get<LayerGroup>(entity);
-				else
-					layerNo = 0;
+				LayerGroup layerNo = (m_registry.has<LayerGroup>(entity) ? m_registry.get<LayerGroup>(entity) : 0);
 
 				// Place sprites into queue for sorting later.
 				renderQueue.emplace_back(std::make_tuple(spr, trans.rotation, destRect, layerNo));
 			});
 
 			// Sort the sprites based on priority, higher number means rendered later. Same layer may fight 
-			std::sort(renderQueue.begin(), renderQueue.end(), [](const auto& lhs, const auto& rhs) {
+			std::stable_sort(renderQueue.begin(), renderQueue.end(), [](const auto& lhs, const auto& rhs) {
 				return std::get<3>(lhs) < std::get<3>(rhs);
 			});
 
 			// Render
-			for (auto i : renderQueue) {
-				auto & spr = std::get<0>(i);
-				auto & rot = std::get<1>(i);
-				auto & destRect = std::get<2>(i);
+			for (auto& [spr, rot, destRect, lg] : renderQueue)
 				spr.texture->render(spr.srcRect, destRect, rot);
-			}
 		}
 
 	private:
-		SDL_Renderer * renderer;
+		SDL_Renderer* renderer;
 	};
 }
