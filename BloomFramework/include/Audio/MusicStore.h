a167827a64e75e5e4cf6f033cdc6700ed2ab6f58
Anatoly Pitikin <xapdkop@xapdkop.space>
2019-01-27 20:34:54 +0300

    [MusicStore, SoundStore] Removed comments (old code)

--- BloomFramework/include/Audio/MusicStore.h
+++ BloomFramework/include/Audio/MusicStore.h
@@ -15,27 +15,4 @@ namespace bloom::audio {
 			return m_store.try_emplace(filePath, std::make_shared<MusicTrack>(filePath)).first->second;
 		}
 	};
-
-	//class BLOOMFRAMEWORK_API MusicStore {
-	//public:
-	//	static MusicStore& store();
-
-	//	TrackPtr load(const std::filesystem::path& filePath);
-	//	TrackPtr at(const std::filesystem::path& filePath) const;
-	//	TrackPtr find(const std::filesystem::path& filePath) const noexcept;
-	//	void unload(const std::filesystem::path& filePath);
-	//	void unloadAll() noexcept;
-
-	//	TrackPtr operator[](const std::filesystem::path& key) const noexcept;
-
-	//private:
-	//	MusicStore() = default;
-	//	MusicStore(const MusicStore&) = delete;
-	//	MusicStore(MusicStore&&) = delete;
-	//	MusicStore& operator=(const MusicStore&) = delete;
-	//	MusicStore& operator=(MusicStore&&) = delete;
-	//	~MusicStore() = default;
-
-	//	std::unordered_map<std::filesystem::path, TrackPtr, HashPath> m_store;
-	//};
 }
--- BloomFramework/include/Audio/SoundStore.h
+++ BloomFramework/include/Audio/SoundStore.h
@@ -15,27 +15,4 @@ namespace bloom::audio {
 			return m_store.try_emplace(filePath, std::make_shared<SoundChunk>(filePath)).first->second;
 		}
 	};
-
-	//class BLOOMFRAMEWORK_API SoundStore {
-	//public:
-	//	static SoundStore& store();
-
-	//	ChunkPtr load(const std::filesystem::path& filePath);
-	//	ChunkPtr at(const std::filesystem::path& filePath) const;
-	//	ChunkPtr find(const std::filesystem::path& filePath) const noexcept;
-	//	void unload(const std::filesystem::path& filePath);
-	//	void unloadAll() noexcept;
-
-	//	ChunkPtr operator[](const std::filesystem::path& key) const noexcept;
-
-	//private:
-	//	SoundStore() = default;
-	//	SoundStore(const SoundStore&) = delete;
-	//	SoundStore(SoundStore&&) = delete;
-	//	SoundStore& operator=(const SoundStore&) = delete;
-	//	SoundStore& operator=(SoundStore&&) = delete;
-	//	~SoundStore() = default;
-
-	//	std::unordered_map<std::filesystem::path, ChunkPtr, HashPath> m_store;
-	//};
 }
