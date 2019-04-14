4c94688414e4c1cd4ddef69e8a1b69fd0ceb5ede
Anatoly Pitikin <xapdkop@xapdkop.space>
2019-04-14 14:58:03 +0300

    Make Drawable's ctor protected instead of using pseudo pure-virtual dtor

--- BloomFramework/include/Graphics/Drawable.h
+++ BloomFramework/include/Graphics/Drawable.h
@@ -5,12 +5,13 @@
 namespace bloom::graphics {
 	class BLOOMFRAMEWORK_API Drawable {
 	public:
-		Drawable(SDL_Renderer* const& targetRenderer);
-		virtual ~Drawable() = 0;
+		virtual ~Drawable();
 
 		virtual void render(std::optional<SDL_Rect> srcRect, SDL_Rect destRect, SDL_RendererFlip flip = SDL_FLIP_NONE);
 
 	protected:
+		explicit Drawable(SDL_Renderer* const& targetRenderer);
+
 		SDL_Renderer* const& c_renderer;
 		SDL_Texture* m_texture = nullptr;
 	};
